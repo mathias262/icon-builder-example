e066ff2466578b596fd6721196ec6774be67aa42
Mathias Iversen <mathias.iversen@advisa.se>
2020-03-17 09:03:45 +0100

    removed react component output

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,4 @@
 const createModulePackage = require('./createModulePackage');
-const createReactPackage = require('./createReactPackage');
 const del = require('del')
 const fs = require('fs-extra');
 const path = require('path')
@@ -10,7 +9,7 @@ const BUILD_PATH = path.join(__dirname, '..', 'build');
 
 // Build the npm packages
 const createPackages = (svgDataList) => {
-  const packagers = [ createReactPackage, createModulePackage ]; // list other packagers here
+  const packagers = [ createModulePackage ]; // list other packagers here
   const packages = packagers.map((packager) => packager(svgDataList, version));
   del.sync(BUILD_PATH);
   packages.forEach((pack) => {
